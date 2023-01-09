b48ccab8a68955dc9b64a84185b3d99219905b09
dottgonzo <dev@dariocaruso.info>
2023-01-09 01:37:17 +0100

    wip

---
+++ index.js
@@ -0,0 +1,124 @@
+export default class WebrctClient {
+    constructor(options) {
+        this.terminated = false;
+        this.restartPause = 2000;
+        this.ws = null;
+        this.pc = null;
+        this.restartTimeout = null;
+        if (!options?.videoElement)
+            throw new Error('videoElement is required');
+        if (!options.wsUri)
+            throw new Error('wsUri is required');
+        this.wsUri = options.wsUri;
+        this.videoElement = options.videoElement;
+        this.onOnline = options.onOnline;
+        this.onOffline = options.onOffline;
+    }
+    start() {
+        console.log('connecting');
+        this.ws = new WebSocket(this.wsUri);
+        this.ws.onerror = () => {
+            console.log('ws error');
+            if (this.ws === null) {
+                return;
+            }
+            this.ws.close();
+            this.ws = null;
+        };
+        this.ws.onclose = () => {
+            console.log('ws closed');
+            this.ws = null;
+            this.onOffline();
+            this.scheduleRestart();
+        };
+        this.ws.onmessage = msg => this.onIceServers(msg);
+    }
+    onIceServers(msg) {
+        if (this.ws === null) {
+            return;
+        }
+        const iceServers = JSON.parse(msg.data);
+        this.pc = new RTCPeerConnection({
+            iceServers
+        });
+        this.ws.onmessage = msg => this.onRemoteDescription(msg);
+        this.pc.onicecandidate = evt => this.onIceCandidate(evt);
+        this.pc.oniceconnectionstatechange = () => {
+            if (this.pc === null) {
+                return;
+            }
+            console.log('peer connection state:', this.pc.iceConnectionState);
+            switch (this.pc.iceConnectionState) {
+                case 'connected':
+                    this.pc.onicecandidate = undefined;
+                    this.ws.onmessage = undefined;
+                    this.ws.onerror = undefined;
+                    this.ws.onclose = undefined;
+                    // do not close the WebSocket connection
+                    // in order to allow the other side of the connection
+                    // to switch to the "connected" state before WebSocket is closed.
+                    this.onOnline();
+                    break;
+                case 'disconnected':
+                    this.scheduleRestart();
+                    this.onOffline();
+            }
+        };
+        this.pc.ontrack = evt => {
+            console.log('new track ' + evt.track.kind);
+            this.videoElement.srcObject = evt.streams[0];
+        };
+        const direction = 'sendrecv';
+        this.pc.addTransceiver('video', { direction });
+        this.pc.addTransceiver('audio', { direction });
+        this.pc.createOffer().then(desc => {
+            if (this.pc === null || this.ws === null) {
+                return;
+            }
+            this.pc.setLocalDescription(desc);
+            console.log('sending offer');
+            this.ws.send(JSON.stringify(desc));
+        });
+    }
+    onRemoteDescription(msg) {
+        if (this.pc === null || this.ws === null) {
+            return;
+        }
+        this.pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(msg.data)));
+        this.ws.onmessage = msg => this.onRemoteCandidate(msg);
+    }
+    onIceCandidate(evt) {
+        if (this.ws === null) {
+            return;
+        }
+        if (evt.candidate !== null) {
+            if (evt.candidate.candidate !== '') {
+                this.ws.send(JSON.stringify(evt.candidate));
+            }
+        }
+    }
+    onRemoteCandidate(msg) {
+        if (this.pc === null) {
+            return;
+        }
+        this.pc.addIceCandidate(JSON.parse(msg.data));
+    }
+    scheduleRestart() {
+        if (this.terminated) {
+            return;
+        }
+        if (this.ws !== null) {
+            this.ws.close();
+            this.ws = null;
+        }
+        if (this.pc !== null) {
+            this.pc.close();
+            this.pc = null;
+        }
+        this.restartTimeout = window.setTimeout(() => {
+            this.restartTimeout = null;
+            this.start();
+        }, this.restartPause);
+    }
+}
+//# sourceMappingURL=index.js.map
